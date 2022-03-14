f2fa5ccac4ae401dd728b1985e3f64c5cc4249b3
ZhuangZheAn <97737497+ZhuangZheAn@users.noreply.github.com>
2022-03-14 22:55:35 +0800

    HW3 (updated)

---
+++ 109590008_HW3/109590008_HW3(2.1)/app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.twoactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * SecondActivity defines the second activity in the app. It is
+ * launched from an intent with a message, and sends an intent
+ * back with a second message.
+ */
+public class SecondActivity extends AppCompatActivity {
+    // Unique tag for the intent reply.
+    public static final String EXTRA_REPLY =
+            "com.example.android.hellotoast.extra.REPLY";
+
+    // EditText for the reply.
+    private EditText mReply;
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.message);
+        textView.setText(message);
+    }
+
+}
